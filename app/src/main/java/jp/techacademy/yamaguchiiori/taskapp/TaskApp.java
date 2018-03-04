aa3b0af987cbabb5a97c7a548a1d160d6c4cdf07
vismalk <goodluck1043@gmail.com>
2018-03-04 22:28:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yamaguchiiori/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yamaguchiiori.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
